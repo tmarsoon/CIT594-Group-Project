801d73c65f9b537de07551da9ef9868c877af566
imellah12 <167009671+imellah12@users.noreply.github.com>
2024-04-14 17:34:48 +0200

    Update Logger.java

--- src/edu/upenn/cit594/logging/Logger.java
+++ src/edu/upenn/cit594/logging/Logger.java
@@ -1,5 +1,66 @@
 package edu.upenn.cit594.logging;
 
 public class Logger {
+private static Logger instance;
+	private PrintStream output;
 
+	private Logger() {
+		this.output = System.err;
+	}
+	
+	public static Logger getInstance() {
+		if (instance == null) {
+			instance = new Logger();
+		}
+		return instance;
+	}
+	
+	/**
+	 * logging the event
+	 * @param event
+	 */
+	public void logEvent(String event) {
+		// TODO: log event
+	}
+	
+	/**
+	 * sets the output file destination
+	 * @param fileToWrite
+	 */
+	public void changeOutputDest(String fileToWrite) {
+		//TODO implement 
+	}
+	
+	/**
+	 * creates the time stamp to log
+	 */
+	public void timeStampLog() {
+		//TODO implement
+	}
+	
+	/**
+	 * logs the command line arguments
+	 * @param args
+	 */
+	public void logCommandLineArgs(String[] args) {
+		//TODO implement
+	}
+	
+	
+	/**
+	 * logs the name of the input file
+	 * @param inputFile
+	 */
+	public void logInputFile(String inputFile) {
+		//TODO implement
+	}
+	
+	/**
+	 * logs the user input/responses
+	 * @param userInput
+	 */
+	public void logUserInput(String userInput) {
+		//TODO implement
+	}
+	
 }
