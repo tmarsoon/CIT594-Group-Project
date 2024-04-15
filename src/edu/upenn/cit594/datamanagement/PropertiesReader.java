bc08754ba3b2c05d0d1bdd82bdd4b30651079f68
tonymarsalla <marsalla@seas.upenn.edu>
2024-04-14 20:04:20 -0500

    commit

--- src/edu/upenn/cit594/datamanagement/PropertiesReader.java
+++ src/edu/upenn/cit594/datamanagement/PropertiesReader.java
@@ -1,5 +1,30 @@
 package edu.upenn.cit594.datamanagement;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.TreeMap;
+
+import edu.upenn.cit594.logging.Logger;
+
+
 public class PropertiesReader {
 
-}
+
+	    int code = 0;
+	    double livableArea = 0;
+	    double marketValue = 0;
+	    
+
+	    int zipCodePos = -1;
+	    int totalLivableAreaPos = -1;
+	    int marketValuePos = -1;
+
+	    
+	    public PropertiesReader(String filename, Logger logger) {
+	        super(filename, logger);
+	    }
+
+	   
+
