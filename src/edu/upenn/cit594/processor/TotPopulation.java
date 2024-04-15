c133cf38f8df0fc88c49002f77924038fd400cbf
imellah12 <167009671+imellah12@users.noreply.github.com>
2024-04-15 16:14:30 +0200

    Update TotPopulation.java

--- src/edu/upenn/cit594/processor/TotPopulation.java
+++ src/edu/upenn/cit594/processor/TotPopulation.java
@@ -1,1 +1,30 @@
+package edu.upenn.cit594.processor;
+
+import java.io.IOException;
+import java.util.List;
+
+import edu.upenn.cit594.datamanagement.PopulationReader;
+
+public class TotPopulation {
+
+	public static int totalPopCalculator(String populationFile ) {
+		
+		int totPop = 0;
+		
+		// using the Population reader class to be implemented in data management
+		PopulationReader populationReader = new PopulationReader();
+		// adding the pop values to a list using the readPopulation() method to be implemented
+		List <Integer > populationData = populationReader.readPopulation(populationFile);
+		
+		// adding the values together
+		
+		for (int population :populationData ) {
+			totPop += population;
+		}
+		
+		return totPop;
+	}
+
+
+}
 
