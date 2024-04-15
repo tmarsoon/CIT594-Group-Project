20fa473f60bb935f96bd72240979108b8adbdcec
imellah12 <167009671+imellah12@users.noreply.github.com>
2024-04-15 15:36:54 +0200

    Update UserInterface.java

--- src/edu/upenn/cit594/ui/UserInterface.java
+++ src/edu/upenn/cit594/ui/UserInterface.java
@@ -4,42 +4,43 @@ import java.util.Scanner;
 
 
 public class UserInterface {
+	private Scanner scanner;
+	
+	public UserInterface() {
+		this.scanner = new Scanner(System.in);
+	}
 		
+	
+	private int readInput() {
+		while (!scanner.hasNextInt()) {
+			System.out.println("Invalid input. Please enter a number between 0 and 7. > ");
+			scanner.next();
+		}
+		int input = scanner.nextInt();
+		scanner.nextLine();
+		return input;
+	}
+	
+	
 	public int requestUserInput() {
 		
-		Scanner scanner = new Scanner(System.in);
-		int input;
-		
-		do {
-			System.out.println("Which action would you like to perform?");
-			System.out.println("0. Exit the program.\n"
-					+ "1. Show the available actions.\n"
-					+ "2. Show the total population for all ZIP Codes.\n"
-					+ "3. Show the total vaccinations per capita for each ZIP Code for the specified date.\n" 
-					+ "4. Show the average market value for properties in a specified ZIP Code.\n"
-					+ "5. Show the average total livable area for properties in a specified ZIP Code.\n"
-					+ "6. Show the total market value of properties, per capita, for a specified ZIP Code.\n"
-					+ "7. Show the results of your custom feature.");
+			displayMenu();
 			System.out.print("> ");
 			System.out.flush(); 
-			
-			if (scanner.hasNextInt()) {
+			return readInput();
+	}
+	
+	
+	public void displayMenu() {
+		System.out.println("Which action would you like to perform?");
+		System.out.println("0. Exit the program.\n"
+				+ "1. Show the available actions.\n"
+				+ "2. Show the total population for all ZIP Codes.\n"
+				+ "3. Show the total vaccinations per capita for each ZIP Code for the specified date.\n" 
+				+ "4. Show the average market value for properties in a specified ZIP Code.\n"
+				+ "5. Show the average total livable area for properties in a specified ZIP Code.\n"
+				+ "6. Show the total market value of properties, per capita, for a specified ZIP Code.\n"
+				+ "7. Show the results of your custom feature.");
 		
-				// get the input
-				input = scanner.nextInt();
-		
-				if ((input >= 0 && input <= 7)) {
-					break;
-				}
-		
-			} else {
-				System.out.println("Invalid input. Please enter a number between 0 and 7. > ");
-				input = scanner.nextInt();
-			}
-			
-		} while (true);
-		
-		scanner.close();
-		return input;
 	}
 }
