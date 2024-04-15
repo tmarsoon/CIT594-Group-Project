5dc8247f0f5cb11ccabcdcfab2af52f027330d1a
imellah12 <167009671+imellah12@users.noreply.github.com>
2024-04-15 16:15:29 +0200

    Update and rename PopulationReader to PopulationReader.java

---
+++ src/edu/upenn/cit594/datamanagement/PopulationReader.java
@@ -0,0 +1,13 @@
+package edu.upenn.cit594.datamanagement;
+
+import java.util.List;
+
+public class PopulationReader {
+
+	public List<Integer> readPopulation(String populationFile) {
+		// TODO IMPLEMENT
+		return null;
+	}
+
+}
+
