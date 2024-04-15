1b7566941bbb73a51803786132550db73f4913ea
tonymarsalla <marsalla@seas.upenn.edu>
2024-04-14 20:10:18 -0500

    initial

---
+++ src/edu/upenn/cit594/datamanagement/FileSuperLogger.java
@@ -0,0 +1,27 @@
+package edu.upenn.cit594.datamanagement;
+
+import edu.upenn.cit594.logging.Logger;
+
+/*
+ * Unlike the solo project, 
+ */
+public abstract class FileSuperLogger {
+	    
+	//creating two variables with protected as the data type so we can access these within other classes of the same package
+	    protected String filename;
+	    protected Logger logger;
+	    
+	    public FileSuperLogger(String filename, Logger logger) {
+	        this.filename = filename;
+	        this.logger = logger;
+	    }
+	
+	    protected void openLogger() {
+	        if (logger == null) {
+	            return;
+	        }
+	        logger.openLogger(filename);
+	    }
+
+	}
+
