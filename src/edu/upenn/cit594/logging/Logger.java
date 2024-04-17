b94b920a872d52209da4df588434d076ca808128
tonymarsalla <marsalla@seas.upenn.edu>
2024-04-16 20:20:29 -0500

    Update.

--- src/edu/upenn/cit594/logging/Logger.java
+++ src/edu/upenn/cit594/logging/Logger.java
@@ -1,11 +1,18 @@
 package edu.upenn.cit594.logging;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+
 public class Logger {
 	private static Logger instance;
 	private PrintStream output;
 
+
 	private Logger() {
+		//Initializing the logger to have a standard error output since the the main method may be invoked multiple times
 		this.output = System.err;
+		
 	}
 	
 	public static Logger getInstance() {
@@ -15,36 +22,57 @@ public class Logger {
 		return instance;
 	}
 	
-	/**
-	 * logging the event
-	 * @param event
-	 */
+	 /**
+     * 	Logging an event with a timestamp
+     * 	The timestamp here will be prnted before the message is sent to the logger
+     * @param event  The event message
+     */
 	public void logEvent(String event) {
-		// TODO: log event
+		String timestamp = String.valueOf(System.currentTimeMillis());
+	        output.println(timestamp + " " + event);
 	}
 	
 	/**
-	 * sets the output file destination
-	 * @param fileToWrite
+	 * sets the output file destination.
+	 * If the previous destination is a file, it will be closed while switching.
+	 * @param fileToWrite  File that the Logger wirtes to
 	 */
 	public void changeOutputDest(String fileToWrite) {
-		//TODO implement 
-	}
+		//if a file exist, ouput should be closed
+		if (output != System.err) {
+			output.close();
+		}
+		//if the file is empty or contains no content, ouput the error and return void
+		if (fileToWrite == null || fileToWrite.isEmpty()) {
+			output = System.err;
+			return;
+		}
+	//if necessary, attempt to append the log file
+	  try {
+          //append to the specified log file
+          output = new PrintStream(new FileOutputStream(fileToWrite, true));
+      } catch (FileNotFoundException e) {
+          // If unable to create the log file, revert to standard error
+          System.err.println("Unable to open log file: " + fileToWrite);
+          output = System.err;
+      }
+  }
 	
-	/**
-	 * creates the time stamp to log
-	 */
-	public void timeStampLog() {
-		//TODO implement
-	}
 	
 	/**
 	 * logs the command line arguments
 	 * @param args
 	 */
 	public void logCommandLineArgs(String[] args) {
-		//TODO implement
-	}
+		//creating a stringbuilder object since SB can be modified
+		//append() will automatically intiate a capacity update to the object
+		  StringBuilder stringBuild = new StringBuilder("Command Line Arguments: ");
+		  //creating a for loop of single entry arguments with a space in between
+		  for (String arg : args) {
+		      stringBuild.append(arg).append(" ");
+		        }
+		        logEvent(stringBuild.toString());
+		    }
 	
 	
 	/**
@@ -52,7 +80,7 @@ public class Logger {
 	 * @param inputFile
 	 */
 	public void logInputFile(String inputFile) {
-		//TODO implement
+		logEvent("Input File: " + inputFile);
 	}
 	
 	/**
@@ -60,7 +88,7 @@ public class Logger {
 	 * @param userInput
 	 */
 	public void logUserInput(String userInput) {
-		//TODO implement
+		 logEvent("User Input/Response: " + userInput);
 	}
 	
 }
